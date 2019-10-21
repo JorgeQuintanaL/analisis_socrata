7a0e0e1243c054a29afad5878e40051aa9b2615f
Ivonne Lissete Alvarez Mojica <ilalvarezm@MacBook-Air-de-Ivonne.local>
2019-10-20 21:13:10 -0500

    Commit sin informacion sensible

--- configure_docker.sh
+++ configure_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 docker pull mariadb
-docker run --name dev_mariadb -e MYSQL_ROOT_PASSWORD=root1234 -e MYSQL_USER=quantomm_dev -e MYSQL_PASSWORD=quantomm_dev -e MYSQL_DATABASE=socrata -v /home/jorgequintana/Documents/Quantomm/modelodatosabiertos/data/MariaDB/:/var/lib/mysql -d mariadb
+docker run --name dev_mariadb -e MYSQL_ROOT_PASSWORD=XXXX -e MYSQL_USER=XXXX -e MYSQL_PASSWORD=XXXX -e MYSQL_DATABASE=XXXX -v XXXX:/var/lib/mysql -d mariadb
 docker pull mongo
-docker run -d --name dev_mongo -e MONGO_INITDB_ROOT_USERNAME=quantomm_dev -e MONGO_INITDB_ROOT_PASSWORD=quantomm_dev mongo
+docker run -d --name dev_mongo -e MONGO_INITDB_ROOT_USERNAME=XXXX -e MONGO_INITDB_ROOT_PASSWORD=XXXX mongo
